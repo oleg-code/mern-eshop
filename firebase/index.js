f7e424a49388f88c77fe795e57befe14e94efc22
login <oleg_grygorenko@bigmir.net>
2021-01-04 11:06:39 +0200

    first commit

---
+++ firebase/index.js
@@ -0,0 +1,10 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mern-101fb.firebaseio.com"
+});
+
+module.exports = admin;
